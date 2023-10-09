fd5a46c4867c56d26c3fbfdd156b845c1d9deaf3
Amit Kumar Singh <aksofficial991@gmail.com>
2023-10-09 14:49:23 +0530

    Product Feature

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+        int c = a*b;
+        System.out.println("Multiplication of a and b is : "+ c);
+    }
+}
