8a7c29a6b3c8ea785247ab89c523f293f3781e45
Amit Kumar Singh <aksofficial991@gmail.com>
2023-10-09 15:34:09 +0530

    While Loop Commit

---
+++ whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int i = 0; //initialize the variable i to 0
+        while(i<10){
+            System.out.println("Hello");
+            i++;
+        }
+    }
+    
+}
