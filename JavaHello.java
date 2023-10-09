e03423e026125997d61a2ccb048a9961dec4860f
Amit Kumar Singh <aksofficial991@gmail.com>
2023-10-09 14:36:25 +0530

    Adding All Files

--- JavaHello.java
+++ JavaHello.java
@@ -1,7 +1,12 @@
 public class JavaHello{
 
 
+    
     public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int c =a+b;//c is the sum of two numbers.
+        System.out.println(c);
         System.out.println("Hello Java");
     }
 }
